d756b636b296615dbe1062ad2dcc27ce0afd2aef
rc15213638835 <935168890@qq.com>
2019-04-08 10:06:26 +0800

    Version_1.0

--- src/ExcptionDemo3.java
+++ src/ExcptionDemo3.java
@@ -14,7 +14,7 @@
 
 自定义异常。
 
-需求：在本程序中，对于除数时-1，也视为时错误的是无法进行运算的。
+需求：在本程序中，对于除数是-1，也视为时错误的是无法进行运算的。
 那么就需要对这个问题进行自定义的描述。
 
 当在函数内部出现了throw抛出异常对象，那么就必须要给出对应的处理动作。
@@ -68,7 +68,7 @@ class FuShuException extends Exception{
 class Demo{
     int div(int a,int b)throws FuShuException{//在功能上通过throws的关键字声明了该功能有可能会出现问题
         if (b<0)
-            throw new FuShuException("出现了除数是负数的情况 /by fushu",b);//手动通过shrow关键字抛出一个自定义对象
+            throw new FuShuException("出现了除数是负数的情况 /by fushu",b);//手动通过throw关键字抛出一个自定义对象
 
         return a/b;
     }
@@ -78,7 +78,7 @@ public class ExcptionDemo3 {
         Demo d = new Demo();
 
         try {
-            int x = d.div(4,-1);
+            int x = d.div(4,-11);
             System.out.println("x="+x);
         } catch (FuShuException e) {
             System.out.println(e.toString());
