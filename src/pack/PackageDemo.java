bd0ba5452d6b36c02fd221d261e10aba824fe6c8
rc15213638835 <935168890@qq.com>
2019-04-16 16:26:25 +0800

    Version_1.0

---
+++ src/pack/PackageDemo.java
@@ -0,0 +1,17 @@
+/**
+ * @program: Study
+ * @Date: 2019/4/16 16:05
+ * @Author: Mr.Ran
+ * @Description:
+ */
+package pack;
+
+import packa.DemoA;
+
+public class PackageDemo {
+    public static void main(String[] args) {
+        DemoA d = new DemoA();
+        d.show();
+
+    }
+}
---
+++ src/packa/DemoA.java
@@ -0,0 +1,13 @@
+package packa;
+
+/**
+ * @program: Study
+ * @Date: 2019/4/16 16:15
+ * @Author: Mr.Ran
+ * @Description:
+ */
+public class DemoA {
+    public void show(){
+        System.out.println("demoa show run");
+    }
+}
